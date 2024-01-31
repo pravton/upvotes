7a520abecba463773ec411a19ca25ed9c6c5f335
Clinton <80900245+pravton@users.noreply.github.com>
2024-01-31 12:33:57 -0500

    added some animation to upvote

--- src/components/Upvote.tsx
+++ src/components/Upvote.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
 
 interface UpvoteProps {
   isSelected: boolean;
@@ -12,17 +13,36 @@ const Upvote: React.FC<UpvoteProps> = ({ onClick, isSelected }) => {
   // class to update the arrow color and background
   const arrowClass = isSelected ? 'selected-arrow' : 'default-arrow';
 
+  const animation = {
+    hidden: { scale: 0, opacity: 0, y: -40 },
+    visible: { 
+      scale: 1,
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+        speed: 0.1
+      }
+    }
+  };
+
 
   return (
-    <button
+    <motion.button
     data-testid="upvote-component"
     title="Upvote"
     type="button"
     className={`${arrowClass} flex items-center justify-center py-2 px-2 rounded-xl w-[50px] h-[50px] cursor-pointer`}
     onClick={onClick}
+    initial='hidden'
+    animate='visible'
+    exit='exit'
+    variants={animation}
     >
       <FontAwesomeIcon className='w-full max-w-[30px] text-xl' icon={faArrowUp} />
-    </button>
+    </motion.button>
   );
 }
 
